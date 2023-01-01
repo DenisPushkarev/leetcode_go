dc30b5bfe78ca4061f6e1eb1fcd4ef3ca209b80b
Denis Pushkarev <dipushkarev@gmail.com>
2023-01-01 15:15:19 +0100

    Solution 88

---
+++ leetcode/solution_88.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	ptr1 := m - 1
+	ptr2 := n - 1
+	for i := n + m; i >= 0; i-- {
+		switch {
+		case ptr1 >= 0 && ptr2 >= 0:
+			if nums1[ptr1] > nums2[ptr2] {
+				nums1[i] = nums1[ptr1]
+				ptr1--
+			} else {
+				nums1[i] = nums2[ptr2]
+				ptr2--
+			}
+		case ptr1 >= 0:
+			nums1[i] = nums1[ptr1]
+			ptr1--
+		case ptr2 >= 0:
+			nums1[i] = nums2[ptr2]
+			ptr2--
+		}
+	}
+}
