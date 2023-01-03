2ebcf506a26e71fba0710e4733e0294ecbc795a2
Denis Pushkarev <dipushkarev@gmail.com>
2023-01-03 18:13:12 +0100

    Solution 283

---
+++ leetcode/solution_283.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func moveZeroes(nums []int) {
+	ptr := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[ptr] = nums[i]
+			ptr++
+		}
+	}
+	for i := ptr; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
